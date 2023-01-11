9bcebf9b8609598f3eca74d6c15fc3afa0667f0d
Sivagopi <perumallasivagopi1234@gmail.com>
2023-01-11 18:00:57 +0530

    Adding files

---
+++ src/NewFile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NewFile() {
+    return (
+        <div>
+            <h1>hello</h1>
+            <p>Welcome to React Js</p>
+        </div>
+    );
+}
+export default NewFile;
